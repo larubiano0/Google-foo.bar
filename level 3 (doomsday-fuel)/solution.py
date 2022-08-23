8f0ed0c776b2df2317b5e49f73ac22335cf024af
larubiano0 <larubiano0@gmail.com>
2022-08-22 19:31:26 -0500

    Level 3 advancements

--- level 3 (doomsday-fuel)/solution.py
+++ level 3 (doomsday-fuel)/solution.py
@@ -1,28 +1,27 @@
 from fractions import Fraction as frac
-from numpy import linalg as LA
-
 
 
 def solution(m):
 
     for i in xrange(len(m)): # for each row of the matrix
 
-        m[i] = scale(m[i]) # scale each row, to make it a prob. space and have a proper transition matrix (right stochastic matrix).
+        m[i] = scale(m[i], i) # scale each row, to make it a prob. space and have a proper transition matrix (right stochastic matrix for a discrete markov chain)
 
-        w, v = LA.eig(m) # get the eigenvalues and eigenvectors of the matrix.
+    '''
+    Here I used the book Adventures in Stochastic Processes (1992),
+    by Sidney Resnick. p 102-116 (Markov Chains: Absorption probabilities)
+    to help me solve this problem.
+    ''' 
 
-        print(v)
-
-        print(w)
     
     
 
-def scale(v): # scale to make the sum of its components = 1, useful for making each row of the matrix 
+def scale(v,i): # scale to make the sum of its components = 1, useful for making each row of the matrix a proper transition matrix
     s = frac(sum(v))
 
     if s==0: 
 
-        return [frac(0) for _ in v] # if the vector is the 0 vector, return the 0 vector. (terminal state)
+        return [frac(0) if j != i else frac(1) for j in xrange(len(v))] # if the row is the 0 vector, the probability of going to itself is 1, otherwise it is 0
 
     nv = [frac(j / s) for j in v]
 
--- level 3 (doomsday-fuel)/solution.test.py
+++ level 3 (doomsday-fuel)/solution.test.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
 
     try: 
 
-        M = [
+        M1 = [
             [0,1,0,0,0,1],  # s0, the initial state, goes to s1 and s5 with equal probability
             [4,0,0,3,2,0],  # s1 can become s0, s3, or s4, but with different probabilities
             [0,0,0,0,0,0],  # s2 is terminal, and unreachable (never observed in practice)
@@ -13,7 +13,13 @@ if __name__ == "__main__":
             [0,0,0,0,0,0],  # s5 is terminal
             ]
 
-        assert solution.solution(M) == [0, 3, 2, 9, 14]
+        M2 = [[0, 2, 1, 0, 0], [0, 0, 0, 3, 4], [0, 0, 0, 0, 0], [0, 0, 0, 0,0], [0, 0, 0, 0, 0]]
+
+        M3 = [[0, 1, 0, 0, 0, 1], [4, 0, 0, 3, 2, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]]
+
+        assert solution.solution(M1) == [0, 3, 2, 9, 14]
+        assert solution.solution(M2) == [7, 6, 8, 21]
+        assert solution.solution(M3) == [0, 3, 2, 9, 14]
 
         print("All tests passed")
 
