5d38aa8f3dc868fb43ebce0cbe37bd6624085087
larubiano0 <larubiano0@gmail.com>
2022-08-26 18:59:59 -0500

    finished 3 - 2

--- level 3 (bomb-baby)/solution.py
+++ level 3 (bomb-baby)/solution.py
@@ -1,3 +1,41 @@
 def solution(x, y):
-    # Your code here
-    pass
+
+    x, y = long(x), long(y)
+
+    s = long(0) #Number of steps
+
+    while (x != 1) and (y != 1):
+
+        if x<y:
+
+            k = y//x 
+
+            s += k
+
+            y -= k*x
+
+        elif y<x:
+
+            k = x//y
+
+            s += k
+
+            x -= k*y
+
+        else: 
+
+            return 'impossible'
+
+        if x < 1 or y < 1:
+
+            return 'impossible'
+
+    if x>y:
+
+        return str(s + x - 1)
+
+    else: 
+
+        return str(s + y - 1)
+
+    
--- level 3 (bomb-baby)/solution.test.py
+++ level 3 (bomb-baby)/solution.test.py
@@ -6,6 +6,8 @@ if __name__ == "__main__":
 
         assert solution.solution('4','7') == '4'
         assert solution.solution('2','1') == '1'
+        assert solution.solution('10','3') == '5'
+        assert solution.solution('2','4') == 'impossible'
 
         print("All tests passed")
 
