d6f089df97ce47046da6cac2da38921e192ce742
larubiano0 <larubiano0@gmail.com>
2022-09-10 18:23:54 -0500

    level 4 - first commit

---
+++ level 4 (distract-the-trainers)/solution.test.py
@@ -0,0 +1,18 @@
+import solution
+
+if __name__ == "__main__":
+
+    try: 
+
+        assert solution.solution() == ''
+        assert solution.solution() == ''
+        assert solution.solution() == ''
+        assert solution.solution() == ''
+
+        print("All tests passed")
+
+    except AssertionError:
+
+        print("FAILED")
+        exit(1)
+    
